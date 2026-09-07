b400ace8aa03bfd42893d010ace5e601a8b5037b
Andrew <andriusmw@users.noreply.github.com>
2026-09-07 11:37:34 +0200

    checking search state

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import { use } from "react"
 import MovieCard from "../components/MovieCard"
+import { useState } from "react"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
 
     const movies = [
         {id:"1",title:"John Wick",year:"2014"},
@@ -8,7 +12,22 @@ function Home() {
         {id:"3",title:"Iron Man",year:"2008"},
     ]
 
+    const HandleSearch = () => {
+        alert(searchQuery)
+    }
+
+
+
     return <div className="home">
+        <form onSubmit={HandleSearch} className="search-form">
+            <input type="text" placeholder="search for movies..." 
+            className="search-input" value={searchQuery} 
+            onChange={(e) => setSearchQuery(e.target.value)}>
+            </input>
+            <button type="submit" className="search-btn">Search</button>
+        </form>
+
+
         <div className="movies-grid">
             {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
         </div>
