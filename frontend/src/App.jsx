dde7b11c2678d46a4b74e1a2c1a495fb5116cf2b
Andrew <andriusmw@users.noreply.github.com>
2026-09-07 12:25:32 +0200

    react router dom working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
 
@@ -8,9 +10,14 @@ function App() {
  
 
   return (
-    <>
-     <Home/>
-    </>
+    // this is now a router component
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorite-empty">
+        <h2>No Favorites movies yet</h2>
+        <p>Add a movie to favorites and it will appear here. </p>
+    </div>
+}
+
+export default Favorites
